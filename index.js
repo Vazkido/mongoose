7c8647e2452cde5b224e3f6a4876d6e42792147d
vazkido <vazkid02@gmail.com>
2025-03-25 19:44:41 -0400

    edited index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import connectDB from "./db.js"
 import dotenv from "dotenv";
+import Customer from "./models/customerModel.js";
 dotenv.config()
 
 const app = express();
@@ -9,7 +10,13 @@ connectDB()
 
 app.get('/:name/:email/:password', async(req, res)=>{
     try {
-        await Customer.create
+        await Customer.create({
+            name:req.params.name,
+            email:req.params.email,
+            password:req.params.password,
+            gender: null
+        })
+        res.send("User Created Successfully")
     } catch (error) {
         
     }
